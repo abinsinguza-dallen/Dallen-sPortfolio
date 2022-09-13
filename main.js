4b5659056071c4665c5054153a4533bd0c49f618
abinsinguza-dallen <dallenabinsinguza@gmail.com>
2022-09-13 13:47:42 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$('.menu-btn').click(function(){
+    $('.navbar .menu').toggleClass("active");
+    $('.menu-btn i').toggleClass("active");
+});
+// typing text animation script
+var typed = new Typed(".typing", {
+    strings: ["Software Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+var typed = new Typed(".typing-2", {
+    strings: ["Software Developer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
